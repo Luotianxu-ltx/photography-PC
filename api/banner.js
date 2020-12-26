0b71549186fb9bc251e6431ccaa0b08321b7b873
罗天旭 <1265062404@qq.com>
2020-12-26 23:12:06 +0800

    首页信息

---
+++ api/banner.js
@@ -0,0 +1,20 @@
+import request from '@/utils/request'
+export default {
+  /**
+   * 查询前2条banner
+   * @returns {*}
+   */
+  getBannerList() {
+    return request({
+      url: `/h5service/bannerfront/getAllBanner`,
+      method: 'get'
+    })
+  },
+
+  getIndexData() {
+    return request({
+      url: `/adminservice/indexfront/index`,
+      method: 'get'
+    })
+  }
+}
